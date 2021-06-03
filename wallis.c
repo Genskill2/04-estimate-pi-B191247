3d13cb4d23bb3fa39e782c5b99913c3c1e4f50ef
B191247 <84988387+B191247@users.noreply.github.com>
2021-06-03 21:07:37 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,10 +23,12 @@ int main(void) {
     }
   }
 }
-float wallis_pi(int n){
+float wallis_pi(int n)
+{
   int i;
   float k,ans=1.0;
-  for(i=1;i<=n;i++){
+  for(i=1;i<=n;i++)
+  {
     k=(float)(4*i*i)/(4*i*i-1);
     ans=ans*k;
   }
